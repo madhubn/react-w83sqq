a60796d8777010df340cdf297145c324614e7565
madhubn <madhubn.17@gmail.com>
2021-03-16 12:38:14 +0530

    1 file modified

--- src/MultiForm.js
+++ src/MultiForm.js
@@ -1,3 +1,39 @@
 import React from "react";
 
-class MultiForm extends React.Component {}
+/* 
+1. We can control the values of multiple input controls bt adding name   attribute to each element.
+2. when you initialize the state in the contructor, use the filed names
+3. To access the field in the event handler use the event. target.name and event.target.value synatx.
+4. To update state in the setState method use brackets [] arround the property name
+*/
+
+class MultiForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      age: null
+    };
+  }
+  onChanges = event => {
+    let name = event.target.name;
+    let val = event.target.value;
+    this.setState({ [name]: val });
+    console.log("this.val", this.state);
+  };
+  render() {
+    return (
+      <form>
+        <h1>
+          Hello {this.state.username} {this.state.age}
+        </h1>
+        <p>Enter your name:</p>
+        <input type="text" onChange={this.onChanges} name="username" />
+        <p>Enter your age:</p>
+        <input type="number" onChange={this.onChanges} name="age" />
+      </form>
+    );
+  }
+}
+
+export default MultiForm;
